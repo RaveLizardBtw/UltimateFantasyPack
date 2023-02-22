491ce1666e868164c1bd71524ad38e0bf4e8f6f0
RaveLizardBtw <68954667+RaveLizardBtw@users.noreply.github.com>
2023-02-22 01:47:55 +0000

    Added More Scripts

---
+++ UFP/Demonic.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using ThunderRoad;
+using System.Collections;
+
+namespace UFP
+{
+    public class DemonicExplosion : MonoBehaviour
+    {
+        Item item;
+        float explodePower, cooldownDuration, fireLerpDuration;
+        private bool imbue = true;
+        SpellCastCharge fireSpell;
+        public void Awake()
+        {
+            item = GetComponent<Item>();
+            fireSpell = Catalog.GetData<SpellCastCharge>("Fire");
+            Imbue();
+            Invoke("Imbue", 0.25f);
+        }
+        public void Setup(float explodePower, float cooldownDuration, float fireLerpDuration)
+        {
+            this.explodePower = explodePower;
+            this.cooldownDuration = cooldownDuration;
+            this.fireLerpDuration = fireLerpDuration;
+        }
+
+        void Imbue()
+        {
+            foreach (Imbue imbue in item.imbues)
+            {
+                imbue.Transfer(fireSpell, imbue.maxEnergy);
+                imbue.energy = imbue.maxEnergy;
+            }
+        }
+
+        public void Explosion()
+        {
+            imbue = false;
+            GameManager.local.StartCoroutine(ItemImbue());
+            Catalog.GetData<ItemData>("DemonicExplosionVFX").SpawnAsync(item1 =>
+            {
+                item1.transform.position = item.flyDirRef.transform.position;
+                item1.rb.isKinematic = true;
+            });
+            foreach (Collider collider in Physics.OverlapSphere(item.transform.position, 10f))
+            {
+                if (collider.GetComponentInParent<Item>() is Item item && !item.mainHandler)
+                {
+                    Vector3 direction = (collider.transform.position - item.gameObject.transform.position).normalized;
+                    collider.attachedRigidbody?.AddForce(direction * explodePower, ForceMode.Impulse);
+                }
+                if (collider.GetComponentInParent<Creature>() is Creature creature && !creature.isPlayer)
+                {
+                    Vector3 direction = (collider.transform.position - this.item.gameObject.transform.position).normalized;
+                    collider.attachedRigidbody?.AddForce(direction * explodePower, ForceMode.Impulse);
+                    creature.TryElectrocute(100, 10, true, true, Catalog.GetData<EffectData>("ImbueFire"));
+                    creature.Kill();
+                    if (!creature.isKilled) creature.ragdoll.SetState(Ragdoll.State.Destabilized);
+                }
+            }
+
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            if (imbue) Imbue();
+            if (item.mainHandler?.playerHand?.controlHand == null) return;
+            if (item.mainHandler.playerHand.controlHand.alternateUsePressed && imbue)
+            {
+                Explosion();
+            }
+        }
+        IEnumerator ItemImbue()
+        {
+            foreach (Imbue imbue in item.imbues) imbue.energy = 0;
+            yield return new WaitForSeconds(cooldownDuration);
+            float time = fireLerpDuration;
+            while (time > 0)
+            {
+                time -= Time.deltaTime;
+                foreach (Imbue imbue in item.imbues)
+                {
+                    imbue.Transfer(fireSpell, 1);
+                    imbue.energy = Mathf.Lerp(imbue.maxEnergy, 0, time / fireLerpDuration);
+                }
+                yield return null;
+            }
+            imbue = true;
+            EffectInstance effect = Catalog.GetData<EffectData>("DemonicChargeEffect").Spawn(item.transform);
+            effect.Play();
+            effect.onEffectFinished += effect2 => effect2.Despawn();
+        }
+    }
+
+
+    public class DemonicExplosionItem : ItemModule
+    {
+        public float explodePower, cooldownDuration, fireLerpDuration;
+        public override void OnItemLoaded(Item item)
+        {
+            item.gameObject.AddComponent<DemonicExplosion>().Setup(explodePower, cooldownDuration, fireLerpDuration);
+            base.OnItemLoaded(item);
+        }
+    }
+}
---
+++ UFP/Draken.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using ThunderRoad;
+using System.Collections;
+
+namespace UFP
+{
+    public class DrakenFlame : MonoBehaviour
+    {
+        Item item;
+        public bool Imbued = false;
+        public float timer = 0f;
+        public float timer1 = 0f;
+        public float force;
+
+
+        public void Awake()
+        {
+            item = GetComponent<Item>();
+            item.OnHeldActionEvent += OnHeld;
+        }
+
+        public void Update()
+        {
+            timer -= Time.deltaTime;
+            timer1 -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                timer = 0f;
+            }
+            if (timer1 <= 0f)
+            {
+                timer1 = 0f;
+                Imbued = false;
+            }
+            if (Imbued == true)
+            {
+                if (timer1 > 0f)
+                {
+                    foreach (Imbue imbue in item.imbues)
+                    {
+                        imbue.Transfer(Catalog.GetData<SpellCastCharge>("Fire").Clone(), imbue.maxEnergy);
+                    }
+                }
+            }
+            else if (Imbued == false)
+            {
+                item.GetCustomReference("FireMesh").gameObject.SetActive(false);
+                item.GetCustomReference("NormalMesh").gameObject.SetActive(true);
+                foreach (Imbue imbue in item.imbues)
+                {
+                    imbue.energy = 0;
+                }
+            }
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (Imbued == true)
+            {
+                if (collision.collider.gameObject.GetComponentInParent<Creature>() != null)
+                {
+                    if (collision.collider.gameObject.GetComponentInParent<Player>() == null)
+                    {
+                        collision.collider.GetComponentInParent<Creature>().ragdoll.SetState(Ragdoll.State.Inert);
+                        collision.collider.GetComponentInParent<Creature>().TryElectrocute(50f, 5, true, true, Catalog.GetData<EffectData>("ImbueFireRagdoll", true));
+                        Vector3 direction = (collision.collider.transform.position - item.gameObject.transform.position).normalized;
+                        collision.collider.attachedRigidbody?.AddForce(direction * force * collision.collider.attachedRigidbody.mass, ForceMode.Impulse);
+                    }
+                }
+
+                if (collision.collider.gameObject.GetComponentInParent<Item>() != null)
+                {
+                    Vector3 direction = (collision.collider.transform.position - item.gameObject.transform.position).normalized;
+                    collision.collider.attachedRigidbody?.AddForce(direction * force * collision.collider.attachedRigidbody.mass, ForceMode.Impulse);
+                }
+            }
+        }
+
+        private void OnHeld(RagdollHand ragdollHand, Handle handle, Interactable.Action action)
+        {
+            if (action == Interactable.Action.AlternateUseStart)
+            {
+                if (timer <= 0f)
+                {
+                    if (Imbued == false)
+                    {
+                        Imbued = true;
+                        timer1 = 10f;
+                        timer = 20f;
+                        item.GetCustomReference("FireMesh").gameObject.SetActive(true);
+                        item.GetCustomReference("NormalMesh").gameObject.SetActive(false);
+                        EffectInstance effect = Catalog.GetData<EffectData>("DrakenChargeEffect").Spawn(item.transform);
+                        effect.Play();
+                        effect.onEffectFinished += effect2 => effect2.Despawn();
+                    }
+                }
+            }
+
+        }
+    }
+
+    public class DrakenFlameItem : ItemModule
+    {
+        public override void OnItemLoaded(Item item)
+        {
+            item.gameObject.AddComponent<DrakenFlame>();
+            base.OnItemLoaded(item);
+        }
+    }
+}
